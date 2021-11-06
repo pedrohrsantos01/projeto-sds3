cc5df8cc7cd9f2d5c33d33671864319c3b9d3e5d
Pedro Santos <pedrohrsantos01@gmail.com>
2021-11-06 13:18:03 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="footer mt-auto py-3 bg-dark">
+          <div className="container">
+            <p className="text-light"> App Desenvolvido por <a href="https://github.com/pedrohrsantos01" target="_blank" rel="noreferrer">Pedro Santos</a></p>
+            <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento Promovido pela escola DevSuperior: <a href="https://github.com/pedrohrsantos01" target="_blank" rel="noreferrer">@pedrohrsantos</a></small></p>
+          </div>
+      </footer>
+    );
+}
+
+export default Footer;
